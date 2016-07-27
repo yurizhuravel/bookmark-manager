dbc528961e76e3fa4fc54270e63cf3c0a0b21378
Thady Condon <thadycondon@hotmail.com>
2016-07-27 11:49:43 +0100

    Created feature test for viewing links, this test now passes.

--- app/app.rb
+++ app/app.rb
@@ -26,6 +26,12 @@ class Bookmarks < Sinatra::Base
     redirect '/links'
   end
 
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,10 @@ class Tag
 
   include DataMapper::Resource
 
+  has n, :links, through: Resource
+
   property :id,     Serial
   property :name,   String
+
 
 end
